2867428694ba9b1ee441f0f353e14746a6f6a920
Sacha FROMENT <sacha.froment@prestashop.com>
2015-05-06 15:56:35 +0200

    [*] Architecture : Broke dependency to Tools::convertPriceFull

--- Adapter/Adapter_Tools.php
+++ Adapter/Adapter_Tools.php
@@ -6,4 +6,9 @@ class Adapter_Tools
 	{
 		return call_user_func_array(array('Tools', 'ps_round'), func_get_args());
 	}
+
+	public function convertPriceFull($amount, Currency $currency_from = null, Currency $currency_to = null)
+	{
+		return call_user_func_array(array('Tools', 'convertPriceFull'), func_get_args());
+	}
 }
